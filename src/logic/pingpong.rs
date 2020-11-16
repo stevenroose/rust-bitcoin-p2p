59f194360c04e8318e603ab783666fa7205d72f8
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2020-11-16 23:53:32 +0000

    Handle first schedule ping where there isn't a last ping

--- src/logic/pingpong.rs
+++ src/logic/pingpong.rs
@@ -9,14 +9,14 @@ use crate::{Config, PeerId, PeerState};
 /// Some statistics we keep on pinging for peers.
 #[derive(Debug, Default)]
 pub struct PingStats {
-	last_nonce: u64,
+	last_nonce: Option<u64>,
 	replied: bool,
 }
 
 /// Handle a scheduled trigger to send a ping to the peer.
 pub fn scheduled_ping(react: &mut Reactions, config: &Config, peer: PeerId, state: &mut PeerState) {
 	// First check if the peer replied to out last ping.
-	if !state.ping_stats.replied {
+	if !state.ping_stats.replied && state.ping_stats.last_nonce.is_some() {
 		debug!("Disconnecting peer {} because he failed to reply to ping", peer);
 		react.disconnect(peer);
 		return;
@@ -26,7 +26,7 @@ pub fn scheduled_ping(react: &mut Reactions, config: &Config, peer: PeerId, stat
 	react.schedule_ping(peer, Instant::now() + config.ping_interval);
 
 	let nonce = rand::random();
-	state.ping_stats.last_nonce = nonce;
+	state.ping_stats.last_nonce = Some(nonce);
 	state.ping_stats.replied = false;
 
 	trace!("Sending ping with nonce {} to peer {}", nonce, peer);
@@ -53,13 +53,20 @@ pub fn handle_pong(react: &mut Reactions, peer: PeerId, state: &mut PeerState, n
 		return;
 	}
 
-	if nonce == state.ping_stats.last_nonce {
-		state.ping_stats.replied = true;
-	} else {
-		debug!(
-			"Peer {} replied with wrong pong nonce ({} instead of {})",
-			peer, nonce, state.ping_stats.last_nonce,
-		);
-		react.disconnect(peer);
+	match state.ping_stats.last_nonce {
+		Some(last_nonce) if last_nonce == nonce => {
+			state.ping_stats.replied = true;
+		}
+		Some(last_nonce) => {
+			debug!(
+				"Peer {} replied with wrong pong nonce ({} instead of {})",
+				peer, nonce, last_nonce,
+			);
+			react.disconnect(peer);
+		}
+		None => {
+			debug!("Peer {} sent an unrequested pong", peer);
+			react.disconnect(peer);
+		}
 	}
 }
